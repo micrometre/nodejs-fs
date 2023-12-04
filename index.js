1f61354d5fef9d2a67868f7568cb81c79715d240
Henok <henokmicrometre@gmail.com>
2023-12-04 07:13:04 +0000

    adde nodmone

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const { watch } = require('node:fs');
 const fs = require('node:fs');
+//import { execFile } from "child_process";
+const execFile = require ("child_process");
+const { spawn } = require('node:child_process');
+const { exec } = require('node:child_process');
+const ls = spawn('ls', ['-lh', '/usr']);
 
 
-watch('./data', (eventType, filename) => {
-  if (filename) {
-    fs.readFile(`./sqlite-data/${filename}`, 'utf-8', (err, data) => {
-      if (err) throw err;
-      console.log(data)
-      return data;  
-    });
+exec('bash scripts/manage.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
   }
-});
+  console.log(`stdout: ${stdout}`);
+  console.log(`stdout: ${stdout}`);
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+}); 
---
+++ scripts/manage.sh
@@ -0,0 +1,17 @@
+ls -la
+ls -la
+ls -la
+ls -la
+pwd
+pwd
+pwd
+pwd
+pwd
+pwd
+pwd
+pwd
+pwd
+pwd
+
+
+
